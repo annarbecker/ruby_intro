85c08c61cb6c8291218263b3f11d44fad7f6b9e5
Anna Becker <anna.r.becker@gmail.com>
2019-02-14 21:02:20 -0800

    Fix formatting

--- lesson_2/lesson_2.rb
+++ lesson_2/lesson_2.rb
@@ -5,12 +5,12 @@
 #  - are not less than 5000
 #  - sorted in reverse order
 class LessonTwo
-	
+
   def array_manipulation
-  	  arr = (1...10000).to_a
-  	    .select{|element| element % 3 == 0}
-	    .reject{|element| element < 5000}
-	    .sort
-	    .reverse
+    arr = (1...10000).to_a
+	  .select{|element| element % 3 == 0}
+      .reject{|element| element < 5000}
+      .sort
+      .reverse
   end
 end
