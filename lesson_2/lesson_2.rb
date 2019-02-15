9cd4b605654b4d350573d81d6dbf5c1c36ed32fb
Anna Becker <anna.r.becker@gmail.com>
2019-02-14 20:36:19 -0800

    Implement lesson 2, array manipulation exercise

---
+++ lesson_2/lesson_2.rb
@@ -0,0 +1,18 @@
+# Lesson 2
+# Write a single chain of commands to find all numbers that
+# * are from an array of numbers 1..10000 inclusive
+# * are divisible by 3 (element %3 == 0)
+# * are not less than 5000
+# * sorted in reverse order
+class LessonTwo
+
+	def array_manipulation
+		arr = (1...10000).to_a
+		  .select{|element| element % 3 == 0}
+		  .reject{|element| element < 5000}
+		  .sort!
+		  .reverse!
+		  
+		# arr
+	end
+end
